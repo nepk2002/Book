ecd01489c589804ec29e59201a89039a2a2e7fdc
nepk2002 <83213858+nepk2002@users.noreply.github.com>
2021-05-28 12:22:19 +0300

    Add files via upload

---
+++ Clock.java
@@ -0,0 +1,18 @@
+package str113;
+
+public class Clock {
+    String time;
+
+    void setTime(String t){
+        time = t;
+    }
+
+     String getTime() {
+        return time;
+    }
+    //    void getTime(){
+//        return time;
+//    }
+
+}
+
